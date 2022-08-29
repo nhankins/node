67836b3f5e2e4e9e180e902aef6dfa9c35eebf54
nhankins <68508674+nhankins@users.noreply.github.com>
2022-08-29 14:52:54 -0400

    variable change

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ app.get('/', (req,res) => {
     res.send("Welcome to the Home Page!")
 });
 
-
-app.listen(3000, () => {
+const port = process.env.port || 3000;
+app.listen(port, () => {
     console.log("Hi");
 });
 

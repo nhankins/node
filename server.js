4b3bc8e5a55fb5228eab70edcab30fcff4412a5b
nhankins <68508674+nhankins@users.noreply.github.com>
2022-08-29 22:11:10 -0400

    added database

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://nicholasjames:WFhOVXxs63vR2liK@cluster0.yqrkicd.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 const app = express();
+app.use(express.json());
 
 
 app.get('/', (req,res) => {
@@ -7,7 +12,41 @@ app.get('/', (req,res) => {
 });
 
 app.get('/api/users', (req, res) => {
-   res.send("users"); 
+    client.connect(err => {
+        const collection = client.db("test").collection("devices");
+        collection.find().toArray((error, documents) => {
+            if(error){
+                throw error;
+            }
+            res.send(documents)
+
+        client.close();
+
+        });
+
+        
+      });
+   
+    
+});
+
+
+app.post('/api/users', (req,res) => {
+    client.connect(err => {
+        const collection = client.db("test").collection("devices");
+        collection.insertOne(req.body, (error, result) => {
+
+            if(error){
+                throw error
+            }
+            res.send(result.insertedId);
+        client.close();
+        });
+        
+
+    });
+    
+
 });
 
 const port = process.env.PORT || 3000;
